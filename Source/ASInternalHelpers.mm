afcac70cf43b01f9e840c12240457471d66eef5d
Oliver Fox <fox.oliverj@gmail.com>
2025-05-14 09:45:17 +0100

    revert scale change

--- Source/ASInternalHelpers.mm
+++ Source/ASInternalHelpers.mm
@@ -181,8 +181,9 @@ CGFloat ASScreenScale()
   static CGFloat __scale = 0.0;
   static dispatch_once_t onceToken;
   dispatch_once(&onceToken, ^{
-    UIGraphicsImageRenderer *renderer = [[UIGraphicsImageRenderer alloc] initWithSize:CGSizeMake(1, 1)];
-    __scale = renderer.image.scale;
+     UIGraphicsBeginImageContextWithOptions(CGSizeMake(1, 1), YES, 0);
+    __scale = CGContextGetCTM(UIGraphicsGetCurrentContext()).a;
+    UIGraphicsEndImageContext();
   });
   return __scale;
 }
